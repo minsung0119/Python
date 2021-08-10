a20523680f811fc1f83852d7888fb4cb79e88f35
곽민성 <86041270+minsung0119@users.noreply.github.com>
2021-08-10 14:25:54 +0900

    8 월10 일 if문 복습

---
+++ wqe.py
@@ -0,0 +1,11 @@
+num1=int(input("정수 1개를 출력해주세요"))
+num2=int(input("정수 한개를 입력해 주세요"))
+if num1>num2:
+    print("큰수는 %d이고,작은수는 %d입니다"%(num1,num2))
+
+elif num1<num2:
+    print("큰수는 %d이고,작은 수는 %d입니다"%(num2,num1))
+else:
+    print("두수는 %d으로 같습니다"%(num1))
+    
+
